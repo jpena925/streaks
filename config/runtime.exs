653a8495b88a4360ec5c1740d284d5f01146ed04
Jack Pena <penajackc@gmail.com>
2025-10-20 09:41:37 -0500

    Fix SSL config and disable DNS clustering for Gigalixir

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,8 +34,7 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
-    ssl: true,
-    ssl_opts: [
+    ssl: [
       verify: :verify_none,
       cacerts: :public_key.cacerts_get()
     ]
@@ -55,7 +54,7 @@ if config_env() == :prod do
   host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
-  config :streaks, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+  config :streaks, :dns_cluster_query, nil
 
   config :streaks, StreaksWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],

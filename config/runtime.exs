133a33a4409529cd24f1820c6c89d854a1ea248c
Jack Pena <penajackc@gmail.com>
2025-10-21 08:09:36 -0500

    revert ssl config

--- config/runtime.exs
+++ config/runtime.exs
@@ -38,8 +38,7 @@ if config_env() == :prod do
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
-    ssl: true,
-    ssl_opts: [
+    ssl: [
       verify: :verify_none,
       cacerts: :public_key.cacerts_get()
     ]

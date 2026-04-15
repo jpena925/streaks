a2f131b967da8ee6df0c21ecf037a916eb0f033c
Jack Pena <penajackc@gmail.com>
2026-04-15 11:55:29 -0500

    completion schema

--- lib/streaks/habits/habit_completion.ex
+++ lib/streaks/habits/habit_completion.ex
@@ -8,6 +8,8 @@ defmodule Streaks.Habits.HabitCompletion do
           id: integer() | nil,
           completed_on: Date.t() | nil,
           quantity: integer() | nil,
+          qualitative_option_id: String.t() | nil,
+          qualitative_color: String.t() | nil,
           habit_id: integer() | nil,
           habit: Habit.t() | Ecto.Association.NotLoaded.t(),
           inserted_at: DateTime.t() | nil,
@@ -17,6 +19,8 @@ defmodule Streaks.Habits.HabitCompletion do
   schema "habit_completions" do
     field :completed_on, :date
     field :quantity, :integer
+    field :qualitative_option_id, :string
+    field :qualitative_color, :string
 
     belongs_to :habit, Habit
 
@@ -26,11 +30,18 @@ defmodule Streaks.Habits.HabitCompletion do
   @spec changeset(t(), map()) :: Ecto.Changeset.t()
   def changeset(habit_completion, attrs) do
     habit_completion
-    |> cast(attrs, [:completed_on, :quantity])
+    |> cast(attrs, [:completed_on, :quantity, :qualitative_option_id, :qualitative_color])
     |> validate_required([:completed_on])
-    |> validate_number(:quantity, greater_than: 0)
+    |> validate_optional_positive_quantity()
     |> unique_constraint([:habit_id, :completed_on],
       message: "Habit already completed on this date"
     )
+  end
+
+  defp validate_optional_positive_quantity(changeset) do
+    case get_field(changeset, :quantity) do
+      nil -> changeset
+      _ -> validate_number(changeset, :quantity, greater_than: 0)
+    end
   end
 end
